dd24d9c918e2af4d5b1a7c423eec9b09c4ae65cb
Paul_Aroo <timmypaul2006@gmail.com>
2022-06-22 15:21:23 +0100

    implement: get and post reqest routes

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const flights = require("./controllers/flightController");
 const models = require("./models/Flight");
 const routes = require("./routes/flightRoute");
 
+const fs = require('fs')
+const flightData = require('./flight.json')
+
 const app = express();
 
 app.use(json());
@@ -11,6 +14,43 @@ app.use(json());
 app.use("/", routes);
 
 const port = process.env.PORT || 3000;
+
+app.post('/book-flight', (req, res) => {
+  if (Object.keys(req.body).length === 0)
+    return res.status(400).json({message: "empty request body"})
+  else {
+    flightData.push({...req.body})
+    const stringedData = JSON.stringify(flightData, null, 2)
+    fs.writeFile('flight.json', stringedData, (err) => {
+      if(err) return res.status(500).json({message: err})
+    })
+    return res.status(200).json({message: "flight booked"})
+  }
+})
+
+app.get('/all-flights', (req, res) => {
+  if (flightData.length) 
+    return res.status(200).json(flightData)
+  else
+    return res.status(400).json({message: "no available data"})
+})
+
+app.get('/single-flight/:id', (req, res) => {
+  const flightId = req.params.id
+  const data = flightData.find(flight => String(flight.id) === flightId)
+  if (data)
+    return res.status(200).json(data)
+  else
+    return res.status(400).json({message: "flight data not found"})
+})
+
+app.put('update-flight', (req, res) => {
+  res.send('we update flight details here')
+})
+
+app.delete('delete-flight', (req, res) => {
+  res.send('flight delete')
+})
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);

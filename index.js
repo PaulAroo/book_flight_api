9569becceed5a465c8250a99785bacd39556606a
Paul_Aroo <timmypaul2006@gmail.com>
2022-06-22 16:28:19 +0100

    implement: put and delete routes

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.get('/all-flights', (req, res) => {
   if (flightData.length) 
     return res.status(200).json(flightData)
   else
-    return res.status(400).json({message: "no available data"})
+    return res.status(400).json({error: "no available data"})
 })
 
 app.get('/single-flight/:id', (req, res) => {
@@ -41,15 +41,30 @@ app.get('/single-flight/:id', (req, res) => {
   if (data)
     return res.status(200).json(data)
   else
-    return res.status(400).json({message: "flight data not found"})
+    return res.status(400).json({error: "flight data not found"})
 })
 
-app.put('update-flight', (req, res) => {
-  res.send('we update flight details here')
+app.put('/update-flight', (req, res) => {
+  const payload = {...req.body}
+  const flightDataIndex = flightData.findIndex(flight =>
+    flight.id === payload.id
+  )
+  flightData.splice(flightDataIndex, 1, payload)
+  const updatedData = JSON.stringify(flightData, null, 2)
+  fs.writeFile('flight.json', updatedData, (err) => {
+    if(err) return res.status(500).json({message: err})
+  })
+  return res.status(200).json({message: "flight data updated"})
 })
 
-app.delete('delete-flight', (req, res) => {
-  res.send('flight delete')
+app.delete('/delete-flight/:id', (req, res) => {
+  const flightId = req.params.id
+  const newData = flightData.filter(flight => String(flight.id) !== flightId)
+  const stringedNewdata = JSON.stringify(newData, null, 2)
+  fs.writeFile('flight.json', stringedNewdata, (err) => {
+    if(err) return res.status(500).json({message: err})
+  })
+  return res.status(200).json({message: "flight data deleted"})
 })
 
 app.listen(port, () => {
